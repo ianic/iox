7cffde66c3ab6dad9327b4878b3608cf97825b56
Igor Anić <igor.anic@gmail.com>
2025-04-05 17:06:27 +0200

    refactor udp

--- src/udp.zig
+++ src/udp.zig
@@ -7,221 +7,193 @@ const io = @import("io.zig");
 
 const log = std.log.scoped(.io_tcp);
 
-pub fn Sender(comptime ClientType: type) type {
-    return struct {
-        const Self = @This();
-
-        allocator: mem.Allocator,
-        io_loop: *io.Loop,
-        client: ClientType,
-        address: std.net.Address = undefined,
-        socket: posix.socket_t = 0,
-        state: State = .closed,
-        max_packet_size: u16 = 508,
-
-        connect_op: io.Op = .{},
-        close_op: io.Op = .{},
-        send_op: io.Op = .{},
-        send_list: std.ArrayList([]const u8), // pending send buffers
-        send_buf: []const u8 = &.{}, // current send buffer
-        send_offset: usize = 0, // bytes already sent from send_buf
-
-        const State = enum {
-            closed,
-            connecting,
-            connected,
-            closing,
-        };
-
-        pub fn init(allocator: mem.Allocator, io_loop: *io.Loop, client: ClientType, address: net.Address) Self {
-            return .{
-                .allocator = allocator,
-                .io_loop = io_loop,
-                .client = client,
-                .address = address,
-                .send_list = std.ArrayList([]const u8).init(allocator),
-            };
-        }
-
-        pub fn deinit(self: *Self) void {
-            self.send_list.deinit();
-        }
-
-        /// Start connect operation. `onConnect` callback will be fired when
-        /// finished.
-        fn connect(self: *Self) void {
-            assert(self.state == .closed);
-            assert(!self.connect_op.active() and !self.send_op.active() and !self.close_op.active());
-            assert(self.socket == 0);
-
-            self.connect_op = io.Op.connect(
-                .{
-                    .socket_type = posix.SOCK.DGRAM | posix.SOCK.CLOEXEC,
-                    .addr = &self.address,
-                },
-                self,
-                onConnect,
-                onConnectFail,
-            );
-            self.io_loop.submit(&self.connect_op);
-            self.state = .connecting;
-        }
-
-        fn onConnect(self: *Self, socket: posix.socket_t) io.Error!void {
-            log.debug("{} connected socket {}", .{ self.address, socket });
-            self.socket = socket;
-            self.state = .connected;
-            self.sendPending();
-        }
-
-        fn onConnectFail(self: *Self, err: ?anyerror) void {
-            if (err) |e| {
-                log.info("{} connect failed {}", .{ self.address, e });
-                self.freeSendList(e);
-                self.close();
-            }
-        }
-
-        pub fn send(self: *Self, buf: []const u8) io.Error!void {
-            try self.send_list.append(buf);
-
-            switch (self.state) {
-                .closed => self.connect(),
-                .connecting, .closing => {},
-                .connected => self.sendPending(),
-            }
-        }
-
-        /// Start send operation for buffers accumulated in send_list.
-        fn sendPending(self: *Self) void {
-            if (self.send_op.active() or
-                (self.send_list.items.len == 0 and self.send_buf.len == 0)) return;
-
-            if (self.send_buf.len == 0) {
-                // get next buffer from send_list
-                self.send_offset = 0;
-                self.send_buf = self.send_list.items[0];
-                _ = self.send_list.orderedRemove(0);
-            }
-
-            // send at most max_packet_size
-            const buf = self.send_buf[self.send_offset..];
-            const n = @min(buf.len, self.max_packet_size);
-            defer self.send_offset += n;
-
-            self.send_op = io.Op.send(self.socket, buf[0..n], self, onSend, onSendFail);
-            self.io_loop.submit(&self.send_op);
-        }
-
-        /// Send operation is completed, release pending resources and notify
-        /// client that we are done with sending their buffers.
-        fn sendCompleted(self: *Self, err: ?anyerror) void {
-            if (err != null or self.send_buf.len == self.send_offset) {
-                self.client.onSend(self.send_buf, err);
-                self.send_buf = &.{};
-                self.send_offset = 0;
-            }
-            if (err) |e| self.freeSendList(e);
-        }
-
-        fn freeSendList(self: *Self, err: anyerror) void {
-            for (self.send_list.items) |buf|
-                self.client.onSend(buf, err);
-            self.send_list.clearAndFree();
-        }
-
-        fn onSend(self: *Self) io.Error!void {
-            self.sendCompleted(null);
-            self.sendPending();
-        }
-
-        fn onSendFail(self: *Self, err: anyerror) io.Error!void {
-            switch (err) {
-                error.BrokenPipe, error.ConnectionResetByPeer, error.ConnectionRefused => {},
-                else => log.err("{} send failed {}", .{ self.address, err }),
-            }
-            self.sendCompleted(err);
-            self.close();
-        }
-
-        fn onCancel(self: *Self, _: ?anyerror) void {
-            self.close();
-        }
-
-        pub fn close(self: *Self) void {
-            if (self.state == .closed) return;
-            if (self.state != .closing) self.state = .closing;
-
-            if (self.connect_op.active() and !self.close_op.active()) {
-                self.close_op = io.Op.cancel(&self.connect_op, self, onCancel);
-                return self.io_loop.submit(&self.close_op);
-            }
-            if (self.socket != 0 and !self.close_op.active()) {
-                self.close_op = io.Op.shutdown(self.socket, self, onCancel);
-                self.socket = 0;
-                return self.io_loop.submit(&self.close_op);
-            }
-
-            if (self.connect_op.active() or
-                self.send_op.active() or
-                self.close_op.active())
-                return;
-
-            self.state = .closed;
-            self.freeSendList(error.Closed);
-            log.debug("{} closed", .{self.address});
-            self.client.onClose();
-        }
-    };
-}
-
-const TestClient = struct {
+pub const Sender = struct {
     const Self = @This();
-    udp: Sender(*Self),
 
-    fn init(self: *Self, allocator: mem.Allocator, io_loop: *io.Loop, address: net.Address) void {
-        self.* = .{
-            .udp = Sender(*Self).init(allocator, io_loop, self, address),
+    /// Handler callbacks
+    pub const VTable = struct {
+        /// send is done, buffers can be released now
+        onSend: ?*const fn (*anyopaque, []posix.iovec_const, ?anyerror) void,
+        /// connection closed, cleanup done, safe to deinit
+        onClose: ?*const fn (*anyopaque) void,
+    };
+
+    handler: *anyopaque,
+    vtable: VTable,
+    io_loop: *io.Loop,
+
+    socket: posix.socket_t = 0,
+    state: State = .closed,
+    op: io.Op = .{},
+    iov: []posix.iovec_const = &.{},
+    msghdr: posix.msghdr_const = .{
+        .iov = undefined,
+        .iovlen = 0,
+        .name = null,
+        .namelen = 0,
+        .control = null,
+        .controllen = 0,
+        .flags = 0,
+    },
+
+    const State = enum {
+        closed,
+        connecting,
+        open,
+        closing,
+    };
+
+    pub fn init(io_loop: *io.Loop, handler: *anyopaque, vtable: VTable) Self {
+        return .{
+            .io_loop = io_loop,
+            .handler = handler,
+            .vtable = vtable,
+            .state = .closed,
         };
     }
 
-    fn onSend(self: *Self, buf: []const u8, err: ?anyerror) void {
-        _ = self;
-        _ = buf;
-        if (err) |e| {
-            log.err("client onSend {}", .{e});
-        }
+    /// Only one send operation can be submitted at a time.
+    pub fn ready(self: *Self) bool {
+        return !self.op.active() and self.state != .closing;
     }
 
-    fn onClose(self: *Self) void {
-        _ = self;
+    pub fn send(self: *Self, iov: []posix.iovec_const, addr: *net.Address) void {
+        if (iov.len == 0) return;
+        assert(self.ready());
+
+        self.iov = iov;
+        self.msghdr.iov = undefined;
+        self.msghdr.iovlen = 0;
+        self.msghdr.name = &addr.any;
+        self.msghdr.namelen = addr.getOsSockLen();
+
+        if (self.socket == 0) return self.createSocke(addr);
+        self.sendPending();
+    }
+
+    fn createSocke(self: *Self, addr: *net.Address) void {
+        self.state = .connecting;
+        self.op = io.Op.createSocket(
+            .{
+                .socket_type = posix.SOCK.DGRAM | posix.SOCK.CLOEXEC,
+                .addr = addr,
+            },
+            self,
+            onSocket,
+            onError,
+        );
+        self.io_loop.submit(&self.op);
+    }
+
+    fn sendPending(self: *Self) void {
+        assert(self.msghdr.iovlen == 0);
+        if (self.iov.len == 0) return;
+        self.msghdr.iov = self.iov.ptr;
+        self.msghdr.iovlen = @intCast(self.iov.len);
+        self.op = io.Op.sendv(self.socket, &self.msghdr, self, onSend, onError);
+        self.io_loop.submit(&self.op);
+    }
+
+    fn onSocket(self: *Self, socket: posix.socket_t) io.Error!void {
+        assert(self.socket == 0);
+        self.socket = socket;
+        if (self.state == .closing) return self.close();
+        self.state = .open;
+        self.sendPending();
+    }
+
+    /// Send operation is completed, release pending resources and notify
+    /// handler that we are done with sending their buffers.
+    fn sendCompleted(self: *Self, err: ?anyerror) void {
+        self.msghdr.iov = undefined;
+        self.msghdr.iovlen = 0;
+        if (self.vtable.onSend) |cb| cb(self.handler, self.iov, err);
+        self.iov = &.{};
+        if (self.state == .closing) return self.close();
+    }
+
+    fn onSend(self: *Self) io.Error!void {
+        self.sendCompleted(null);
+    }
+
+    fn onError(self: *Self, err: anyerror) io.Error!void {
+        self.sendCompleted(err);
+    }
+
+    fn onCancel(self: *Self, _: ?anyerror) void {
+        self.close();
+    }
+
+    pub fn close(self: *Self) void {
+        if (self.state == .closed) return;
+        if (self.state != .closing) self.state = .closing;
+
+        if (self.socket != 0 and !self.op.active()) {
+            self.op = io.Op.closeSocket(self.socket, self, onCancel);
+            self.socket = 0;
+            return self.io_loop.submit(&self.op);
+        }
+
+        if (self.op.active())
+            return;
+
+        self.state = .closed;
+        if (self.vtable.onClose) |cb| cb(self.handler);
     }
 };
 
 const testing = std.testing;
+pub const FixedSendVec = @import("tcp.zig").FixedSendVec;
 
-test {
-    if (true) return error.SkipZigTest;
+test "Sender" {
+    //if (true) return error.SkipZigTest;
     // To run this test first start listening on udp port:
     // $ nc -kluvw 0 localhost 9000
+
+    const Handler = struct {
+        const Self = @This();
+        udp: Sender,
+        send_count: usize = 0,
+        on_close_count: usize = 0,
+
+        fn onSend(ptr: *anyopaque, iov: []posix.iovec_const, err: ?anyerror) void {
+            const self: *Self = @ptrCast(@alignCast(ptr));
+            self.send_count += 1;
+            if (err != null) unreachable;
+            _ = iov;
+            //std.debug.print("onSend: iov.len: {} error: {any}\n", .{ iov.len, err });
+        }
+        fn onClose(ptr: *anyopaque) void {
+            const self: *Self = @ptrCast(@alignCast(ptr));
+            self.on_close_count += 1;
+        }
+    };
+    var handler: Handler = .{ .udp = undefined };
 
     const allocator = testing.allocator;
     var io_loop: io.Loop = undefined;
     try io_loop.init(allocator, .{ .entries = 4, .recv_buffers = 0, .recv_buffer_len = 0 });
     defer io_loop.deinit();
 
-    const address = try net.Address.resolveIp("127.0.0.1", 9000);
+    var addr = try net.Address.resolveIp("127.0.0.1", 9000);
+    handler.udp = .init(&io_loop, &handler, .{
+        .onClose = Handler.onClose,
+        .onSend = Handler.onSend,
+    });
 
-    var client: TestClient = undefined;
-    client.init(allocator, &io_loop, address);
+    var send_vec: FixedSendVec(4) = .{};
+    assert(send_vec.prep("iso medo u ducan\n"));
+    assert(send_vec.prep("nije reko dobar dan\n"));
+    assert(send_vec.prep("ajde medo van nisi reko dobar dan\n"));
+    assert(send_vec.prep("0123456789abcdf" ** 1024));
+    handler.udp.send(send_vec.get(), &addr);
 
-    try client.udp.send("iso medo u ducan\n");
-    try client.udp.send("nije reko dobar dan\n");
-    try client.udp.send("ajde medo van nisi reko dobar dan\n\n");
-
-    for (0..5) |_| {
+    while (true) {
         try io_loop.tick();
+        if (handler.send_count > 0) break;
     }
-    client.udp.close();
+    handler.udp.close();
     try io_loop.drain();
+
+    try testing.expect(handler.on_close_count == 1);
 }
